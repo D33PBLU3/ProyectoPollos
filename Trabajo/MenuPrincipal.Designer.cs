d186b51d1d70d1bd39a458802658345331ca5b5f
ostol <ostolo_1995@hotmail.com>
2017-02-22 17:26:46 -0600

    agrege imagenes

--- Trabajo/MenuPrincipal.Designer.cs
+++ Trabajo/MenuPrincipal.Designer.cs
@@ -78,7 +78,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(311, 80);
+            this.ClientSize = new System.Drawing.Size(308, 79);
             this.Controls.Add(this.btnVentas);
             this.Controls.Add(this.btnConfiguracionUsuarios);
             this.Controls.Add(this.btnProductos);
